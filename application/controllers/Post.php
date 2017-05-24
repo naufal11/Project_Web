84e0c4ddb859e20d02f61e72549e5c3b902f78c6
deva arofi <deva arofi>
2017-05-24 20:50:51 +0700

    Initial commit for poster from deva

--- application/controllers/Post.php
+++ application/controllers/Post.php
@@ -38,12 +38,42 @@ class Post extends CI_Controller{
         'intIdUser' => $this->session->userdata['user']['intIdUser'],
         'title'     => $this->input->post('title'),
         'caption'   => $this->input->post('caption'),
+        'heading'   => $this->input->post('heading'),
+        'mark'      => $this->input->post('mark'),
         'file'      => $file_name,
-        'dtmDate'      => date("Y-m-d H:i:s")
+        'dtmDate'   => date("Y-m-d H:i:s")
       );
       $this->db->insert('lol_post', $data_post);
       redirect("home/");
     }
+  }
+
+  public function response()
+  {
+    $intIdPost = $this->input->post('intIdPost');
+    $response  = $this->input->post('response');
+    $intIdUser = $this->session->userdata['user']['intIdUser'];
+
+    $data_response = array(
+      'intIdPost' => $intIdPost,
+      'intIdUser' => $intIdUser,
+      'response'  => $response
+    );
+    $isAble = $this->db->where('intIdUser', $intIdUser)
+    ->where('intIdPost',$intIdPost)
+    ->get("lol_like_post")->row_array();
+
+    if (!$isAble) {
+      $this->db->insert('lol_like_post', $data_response);
+    }else {
+      $this->db->where('intIdUser', $intIdUser)
+      ->update("lol_like_post", $data_response);
+    }
+
+    $data = $this->db->where("intIdPost", $intIdPost)
+    ->where("response", "1")
+    ->get("lol_like_post")->result_array();
+    echo json_encode(count($data));
   }
 
 }
--- application/views/Homepage.php
+++ application/views/Homepage.php
@@ -1,100 +1,127 @@
 <?php $this->load->view('header_source'); ?>
 <?php $this->load->view('menu'); ?>
 
-    <!-- Page Content -->
-    <br><br><br>
-    <div class="container">
-        <!-- Page Heading -->
-        <div class="row">
+<!-- Page Content -->
+<br><br><br>
+<div class="container">
+  <!-- Page Heading -->
+  <div class="row">
 
-<!-- Modal -->
-            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-              <div class="modal-dialog" role="document">
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
 
-              <?php echo form_open_multipart('post/post_do');?>
+        <?php echo form_open_multipart('post/post_do');?>
 
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="myModalLabel">Pos Mai Lol</h4>
-                  </div>
-                  <div class="modal-body">
-                      <div class="form-group">
-                        <label for="title" class="control-label">Titel:</label>
-                        <input type="text" class="form-control" id="title" name="title">
-                      </div>
-                      <div class="form-group">
-                        <label for="caption" class="control-label">Kepsion:</label>
-                        <textarea class="form-control" id="caption" name="caption" style="width:100%"></textarea>
-                      </div>
-                      <div class="form-group">
-                        <label for="File">Potos:</label>
-                        <input type="file" id="File" name="userfile">
-                      </div>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="Submit" class="btn btn-primary">Save changes</button>
-                  </div>
-                </div>
-              </form>
-              </div>
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">Pos Mai Lol</h4>
+          </div>
+          <div class="modal-body">
+            <div class="form-group">
+              <label for="title" class="control-label">Titel:</label>
+              <input type="text" class="form-control" id="title" name="title">
             </div>
+            <div class="form-group">
+              <label for="heading" class="control-label">Hiding:</label>
+              <input type="text" class="form-control" id="heading" name="heading">
+            </div>
+            <div class="form-group">
+              <label for="mark" class="control-label">Mark:</label>
+              <input type="text" class="form-control" id="mark" name="mark">
+            </div>
+            <div class="form-group">
+              <label for="caption" class="control-label">Kepsion:</label>
+              <textarea class="form-control" id="caption" name="caption" style="width:100%"></textarea>
+            </div>
+            <div class="form-group">
+              <label for="File">Potos:</label>
+              <input type="file" id="File" name="userfile">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="Submit" class="btn btn-primary">Save changes</button>
+          </div>
         </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Page Heading
-                    <small>Secondary Text</small>
-                </h1>
-            </div>
-        </div>
-        <!-- /.row -->
-
-        <?php foreach ($lol_posts as $lol_post): ?>
-        <!-- Project One -->
-        <div class="row">
-            <div class="col-md-7">
-                <a href="#">
-                    <img class="img-responsive" src="<?php echo base_url('assets/upload/').$lol_post['file'] ?>" alt="<?php echo $lol_post['mark'] ?>">
-                </a>
-            </div>
-            <div class="col-md-5">
-                <h3><?php echo $lol_post['title'] ?></h3>
-                <h4><?php echo $lol_post['username'] ?></h4>
-                <p><?php echo $lol_post['caption'] ?></p>
-                <div class="btn btn-primary" onclick="response(\"<?php echo $lol_post['intIdPost'] ?>\",1)" ><span class="fa fa-thumbs-o-up"></span> Lek </div>
-                <div class="btn btn-info" href="#"><span class="fa fa-comment"></span> Komen</div>
-                <div class="btn btn-danger" href="#"><span class="fa fa-thumbs-o-down"></span> Dislek</div>
-            </div>
-        </div>
-        <!-- /.row -->
-        <hr>
-
-      <?php endforeach; ?>
-
-        <!-- Pagination -->
-        <div class="row text-center">
-            <div class="col-lg-12">
-                <button type="button" class="btn btn-info" name="button">
-                  <i class=""></i> More
-                </button>
-            </div>
-        </div>
-
-        <br>
-
-        <!-- /.row -->
-
-        <!-- <hr> -->
-
-        <!-- </footer> -->
-
+      </form>
     </div>
-    <!-- /.container -->
+  </div>
+</div>
+
+<?php foreach ($lol_posts as $lol_post): ?>
+<div class="row">
+  <div class="col-lg-12">
+    <h2 class="page-header"><?php echo $lol_post['heading'] ?>
+      <small><?php echo date_format(date_create($lol_post['dtmDate']),"Y-m-d") ?></small>
+    </h2>
+  </div>
+</div>
+<!-- /.row -->
+
+  <!-- Project One -->
+  <div class="row">
+    <div class="col-md-7">
+      <a href="#">
+        <img class="img-responsive" src="<?php echo base_url('assets/upload/').$lol_post['file'] ?>" alt="<?php echo $lol_post['mark'] ?>">
+      </a>
+    </div>
+    <div class="col-md-5">
+      <h3><?php echo $lol_post['title'] ?></h3>
+      <h4><?php echo $lol_post['username'] ?></h4>
+      <p><?php echo $lol_post['caption'] ?></p>
+      <div class="btn btn-primary" onclick="response(<?php echo $lol_post['intIdPost'] ?>,1)">
+        <span class="fa fa-thumbs-o-up" id="like"></span> Lek
+      </div>
+      <div class="btn btn-info"><span class="fa fa-comment"></span> Komen</div>
+      <div class="btn btn-danger" onclick="response(<?php echo $lol_post['intIdPost'] ?>,0)">
+        <span class="fa fa-thumbs-o-down"></span> Dislek
+      </div>
+    </div>
+  </div>
+  <!-- /.row -->
+  <hr>
+
+<?php endforeach; ?>
+
+<!-- Pagination -->
+<div class="row text-center">
+  <div class="col-lg-12">
+    <button type="button" class="btn btn-info" name="button">
+      <i class=""></i> More
+    </button>
+  </div>
+</div>
+
+<br>
+
+<!-- /.row -->
+
+<!-- <hr> -->
+
+<!-- </footer> -->
+
+</div>
+<!-- /.container -->
 
 
 <?php $this->load->view('footer'); ?>
 
+<script type="text/javascript">
 
-  </body>
+function response(id, value) {
+  $.post("<?php echo site_url('post/response') ?>", {
+    "intIdPost" : id,
+    "response"  : value
+  },
+  function(data) {
+    /*optional stuff to do after success */
+    data = JSON.parse(data);
+    $("#like").html(" "+data);
+  });
+}
+
+</script>
+</body>
 </html>
