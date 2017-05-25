4fb054e4bba07337205c839b9cc5e611bfd8b67d
naufal husain <naufal husain>
2017-05-25 15:39:59 +0700

    Commit view menu

--- application/views/menu.php
+++ application/views/menu.php
@@ -19,14 +19,33 @@
             </li>
             <li class="<?php if($menu == 'timeline'){ echo 'active'; } ?>"><a href="<?php echo site_url('home/') ?>">TAIMLEN</a></li>
             <li class="<?php if($menu == 'my_post'){ echo 'active'; } ?>"><a href="<?php echo site_url('home/my_post') ?>">MAI POS</a></li>
-            <li class="<?php if($menu == 'my_profile'){ echo 'active'; } ?>"><a href="<?php echo site_url('home/my_profile') ?>">MAI PROPIL</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">PAGES <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">BLOG</a></li>
-                <li><a href="<?php echo site_url('users/logout') ?>">LOGOD</a></li>
-              </ul>
-            </li>
+
+            <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="margin-right:15px;">
+              <img src="<?php echo base_url('/assets/user.png') ?>" class="user-image img-circle" style="width: 40px; height: 40px;" alt="User Image">
+              <span class="hidden-xs"> ADMIN</span>
+            </a>
+            <ul class="dropdown-menu" style="margin-right: 15px;">
+              <!-- User image -->
+              <li class="user-header">
+                <img src="<?php echo base_url('/assets/user.png') ?>" class="img-circle" alt="User Image">
+                <p>
+                  ADMIN                  <!--small>Member since Nov. 2012</small-->
+                </p>
+                <p><small>ADMIN</small></p>
+              </li>
+              <!-- Menu Body -->
+              <li class="user-footer">
+                <div class="pull-left">
+                  <a href="<?php echo site_url('home/my_profile')?>" class="btn btn-info btn-flat">MAI PROPIL</a>
+                </div>
+                <div class="pull-right">
+                  <a href="<?php echo site_url('users/logout') ?>" class="btn btn-danger btn-flat">LOGOD</a>
+                </div>
+              </li>
+            </ul>
+          </li>
+
           </ul>
         </div><!--/.nav-collapse -->
       </div>
