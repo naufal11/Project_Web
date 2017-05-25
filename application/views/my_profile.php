f88e03f3b2572738ba607cecec49a84afa3a1eb6
naufal husain <naufal husain>
2017-05-25 16:51:58 +0700

    commit from naufal

--- application/views/my_profile.php
+++ application/views/my_profile.php
@@ -4,27 +4,51 @@
 <br><br><br><br><br>
 <div class="container">
   <div class="row">
-    <div class="box">
-      <div class="col-lg-12">
-        <h2>My Profile</h2>
-        <div class="hline"></div>
-        <div class="box-body">
-          <div class="group">
-            <div class="col-md-2">
-              <div class="list-group">
-                <a class="btn list-group-item"><h4>Profile settings</h4></a>
-                <a class="btn list-group-item"><h4>My post</h4></a>
-                <a class="btn list-group-item"><h4>Morbi leo risus</h4></a>
-              </div>
-            </div>
-            <div class="col-md-10">
 
+    <br>
+        <div class="col-lg-4 hidden-sm hidden-xs">
+            <div class="panel panel-default"  style="box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);">
+                <div class="panel-body">
+                    <div class="media">
+                      <h2>My Profile</h2>
+                      <div class="hline"></div>
+                      <br>
+                        <div align="center">
+                            <img class="thumbnail img-responsive" src="https://lut.im/7JCpw12uUT/mY0Mb78SvSIcjvkf.png" width="300px" height="300px">
+                        </div>
+                        <div class="media-body">
+                            <hr>
+                            <h3><strong>Bio</strong></h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel gravida metus, non ultrices sapien. Morbi odio metus, dapibus non nibh id amet.</p>
+                            <hr>
+                            <h3><strong>Location</strong></h3>
+                            <p>Earth</p>
+                            <hr>
+                            <h3><strong>Gender</strong></h3>
+                            <p>Unknown</p>
+                            <hr>
+                            <h3><strong>Birthday</strong></h3>
+                            <p>January 01 1901</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+          <!-- <div class="group">
+            <div class="col-md-3">
+              <div class="list-group">
+                <a href="#" class="btn list-group-item"><h4>Profile settings</h4></a>
+                <a href="#" class="btn list-group-item"><h4>My post</h4></a>
+                <a href="#" class="btn list-group-item"><h4>Morbi leo risus</h4></a>
+              </div>
+            </div> -->
+            <div class="col-lg-8">
+              <h2>My Post</h2>
+              <div class="hline"></div>
               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
   </div>
 
 </div>
