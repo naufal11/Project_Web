7566ed016a30f9e1cccbb1add447081e51d85266
Wahyu <wahyu@outlook.com>
2017-05-30 14:31:09 +0700

    initial by wahyu firmansyah

--- application/views/change_profile.php
+++ application/views/change_profile.php
@@ -4,8 +4,8 @@
 <br><br><br><br><br>
 <div class="container">
   <div class="row">
-    <div class="col-lg-12">
 
+    <div class="pull-left col-md-12">
       <h2>Change Profile</h2>
       <div class="hline"></div>
       <br>
@@ -21,33 +21,52 @@
         </div>
       </div>
 
-      <div class="col-md-9">
-        <div class="form-group">
-          <label for="" class="col-sm-2">Bio</label>
-          <div class="col-sm-6">
-            <textarea style="width: 100%;" name="bio" placeholder=""></textarea>
+      <div class="pull-right">
+        <div class="col-md-10">
+          <div class="form-group">
+            <label for="" class="col-sm-3">Username</label>
+            <div class="col-sm-8">
+              <input type="text" style="width: 100%;" class="form-control">
+            </div>
           </div>
         </div>
-        <div class="form-group">
-          <label for="" class="col-sm-2">Username</label>
-          <div class="col-sm-6">
-            <input type="text" name="username" value="">
+        <br><br>
+          <div class="col-md-10">
+            <div class="form-group">
+              <label for="" class="col-sm-3">Bio</label>
+              <div class="col-sm-8">
+                <textarea style="width: 100%;" class="form-control" name="bio" placeholder=""></textarea>
+              </div>
+            </div>
+          </div>
+        <br><br><br>
+        <div class="col-md-10">
+          <div class="form-group">
+            <label for="" class="col-sm-3">First & Last Name</label>
+            <div class="col-sm-4">
+              <input type="text" style="width: 100%;" class="form-control" name="firstname" value="">
+            </div>
+            <div class="col-sm-4">
+              <input type="text" style="width: 100%;" class="form-control" name="lastname" value="">
+            </div>
           </div>
         </div>
-        <div class="form-group">
-          <label for="" class="col-sm-2">First & Last</label>
-          <div class="col-sm-3">
-            <input type="text" name="firstname" value="">
+        <div class="col-md-10">
+          <div class="form-group">
+            <label for="" class="col-sm-3">Gender</label>
+            <div class="col-sm-8">
+              <input type="radio"  class="" name="Gender">Laki
+            </div>
+            <div class="col-sm-8">
+              <input type="radio"  class="" name="Gender">Cewe
+            </div>
           </div>
-          <div class="col-sm-3">
-            <input type="text" name="lastname" value="">
-          </div>
+          <br><br>
+          <button type="button" class="btn btn-danger">
+            Update
+          </button>
         </div>
       </div>
-
     </div>
-
   </div>
 </div>
-
-<?php $this->load->view('footer'); ?>
