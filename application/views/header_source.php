a72b47024107eb3300559be923822a5f844206a0
deva arofi <deva arofi>
2017-06-02 14:20:16 +0700

    title commit

--- application/views/header_source.php
+++ application/views/header_source.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="Viewport" content="width=device-width, initial-scale=1">
-    <title>Coba</title>
+    <title>LOLS.</title>
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css") ?>">
